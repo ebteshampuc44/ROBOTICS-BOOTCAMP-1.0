c1794410fc52bef16ddb7c0170a2f4dad20c13ba
ebteshampuc44 <mdebteshamazam@gmail.com>
2025-09-07 12:03:11 +0600

    Refactor Root and main components for improved code clarity; remove commented sections

--- src/Root.jsx
+++ src/Root.jsx
@@ -6,22 +6,20 @@ import Footer from './Components/Footer';
 const Root = () => {
   const location = useLocation();
 
-  // পাথ পরিবর্তন হলে সর্বদা উপরে স্ক্রোল করবে
+
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
+
       <Navbar />
 
-      {/* Main Content */}
       <main className="flex-grow">
         <Outlet />
       </main>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
